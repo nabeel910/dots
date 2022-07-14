83ce698e80fb0d5f0d2b719d3a5a2936ac76470a
nabeel <nabeel910@protonmail.com>
2022-07-14 22:50:37 +0200

    update

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,28 +1,14 @@
-#!/bin/bash
-
-killall sxhkd
-killall picom
-killall -p numlockx
-killall transmission-daemon
-killall /usr/bin/pipewire
-killall /usr/bin/pipewire-pulse
-killall /usr/bin/pipewire-media-session
-killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-killall dunst
-killall clipmenud
-eval "$(dbus-launch --sh-syntax --exit-with-session)"
-
+#!/bin/sh
 setxkbmap -model pc104 -layout us,ar -variant ,, -option grp:alt_shift_toggle &
 xsetroot -cursor_name left_ptr &
 picom -b &
-sxhkd &
 numlockx on &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+pulseaudio &
+/usr/libexec/xfce-polkit &
 dunst &
-feh --bg-scale /home/nabeel/pics/walls/nord-peeks.png
+feh --bg-scale ~/Pictures/walls/bspwmcat.png
 clipmenud &
-xidlehook --not-when-audio --not-when-fullscreen --timer 1200 "slock" "" &
-transmission-daemon &
-/usr/bin/pipewire &
-/usr/bin/pipewire-pulse &
-/usr/bin/pipewire-media-session &
+xss-lock slock &
+sxhkd &
+polybar &
+libinput-gestures &
