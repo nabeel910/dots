cf6569925e31f8112a0e8b05cdc36664434c3f5b
nabeel <nabeel910@protonmail.com>
2022-07-15 14:19:47 +0200

    update

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
+
+
+killall picom
+killall numlockx
+killall /usr/libexec/xfce-polkit
+killall dunst
+killall clipmenud
+killall xss-lock
+killall sxhkd
+killall polybar
+killall libinput-gestures
+killall pipewire
+killall pipewire-media-session
+killall pipewire-pulse
+
+
+
 setxkbmap -model pc104 -layout us,ar -variant ,, -option grp:alt_shift_toggle &
 xsetroot -cursor_name left_ptr &
 picom -b &
 numlockx on &
-pulseaudio &
+pipewire &
+pipewire-media-session &
+pipewire-pulse &
 /usr/libexec/xfce-polkit &
 dunst &
 feh --bg-scale ~/Pictures/walls/bspwmcat.png
